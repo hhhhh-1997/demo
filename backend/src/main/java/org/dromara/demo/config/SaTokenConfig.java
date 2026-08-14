dc7eb4f2b7829576beb6764bc6025d99ef1d28b2
jinuyihui <15172319178@163.com>
2026-08-14 20:28:40 +0800

    fix: 注册 SaInterceptor 使注解鉴权生效

---
+++ backend/src/main/java/org/dromara/demo/config/SaTokenConfig.java
@@ -0,0 +1,21 @@
+package org.dromara.demo.config;
+
+import cn.dev33.satoken.interceptor.SaInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Sa-Token 配置：注册注解式鉴权拦截器，使 @SaCheckLogin/@SaCheckPermission/@SaCheckRole 生效。
+ *
+ * @author demo
+ * @since 2026-08-14
+ */
+@Configuration
+public class SaTokenConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SaInterceptor()).addPathPatterns("/**");
+    }
+}
